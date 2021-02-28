450a4c20432f847305ca6772bc0e6432fbf1d74d
crowe828 <69489498+crowe828@users.noreply.github.com>
2021-02-28 16:59:25 -0500

    Try Catch example.

---
+++ src/TryCatch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TryCatch {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+		int[] a = null;
+		System.out.println(a[0]);
+		} catch(NullPointerException e) {
+			System.out.println("Your array is null");
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("NullPointer");
+		} catch (Exception e) {
+			System.out.println("Something else went wrong.");
+		}
+
+	}
+
+}
